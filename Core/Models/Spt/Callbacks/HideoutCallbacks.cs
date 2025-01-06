af0165c3635339b9442fc0659c6ab156f9243341
CWX <cwxdev@outlook.com>
2025-01-06 16:09:37 +0000

    more types

--- Core/Models/Spt/Callbacks/HideoutCallbacks.cs
+++ Core/Models/Spt/Callbacks/HideoutCallbacks.cs
@@ -2,5 +2,53 @@
 
 public class HideoutCallbacks
 {
-    
+    public ItemEventRouterRepsonse Upgrade(PmcData pmcData, HideoutUpgraderequestData info, string sessionID)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ItemEventRouterRepsonse UpgradeComplete(PmcData pmcData, HideoutUpgradeCompleterequestData info, string sessionID)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ItemEventRouterRepsonse PutItemsInAreaSlots(PmcData pmcData, HideoutPutItemInRequestData info, string sessionID)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ItemEventRouterRepsonse TakeItemsFromAreaSlots(PmcData pmcData, HideoutTakeItemOutRequestData info, string sessionID)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ItemEventRouterRepsonse ToggleArea(PmcData pmcData, HideoutToggleAreaRequestData info, string sessionID)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ItemEventRouterRepsonse SingleProductionStart(PmcData pmcData, HideoutSingleProductionStartRequestData info, string sessionID)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ItemEventRouterRepsonse ScavCaseProductionStart(PmcData pmcData, HideoutScavCaseStartRequestData info, string sessionID)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ItemEventRouterRepsonse ContinuousProductionStart(PmcData pmcData, HideoutContinuousProductionRequestData info, string sessionID)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ItemEventRouterRepsonse TakeProduction(PmcData pmcData, HideoutTakeProductionRequestData info, string sessionID)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Update(int timeSinceLastRun)
+    {
+        throw new NotImplementedException();
+    }
 }
--- Core/Models/Spt/Callbacks/HttpCallbacks.cs
+++ Core/Models/Spt/Callbacks/HttpCallbacks.cs
@@ -2,5 +2,18 @@
 
 public class HttpCallbacks
 {
-    
+    public void Load()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SendImage(string sessionID, object req, object resp, object body)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string GetImage()
+    {
+        throw new NotImplementedException();
+    }
 }
