cfa5f45840e1e5daf1e64066a487ac11b71d9d96
CWX <cwxdev@outlook.com>
2025-01-06 19:05:31 +0000

    more types

--- Core/Callbacks/PresetBuildCallbacks.cs
+++ Core/Callbacks/PresetBuildCallbacks.cs
@@ -2,5 +2,28 @@
 
 public class PresetBuildCallbacks
 {
-    
+    public GetBodyResponseData<List<WeaponBuild>> GetHandbookUserList(string url, object info, string sessionID)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ItemEventRouterResponse SaveWeaponBuild(PmcData pmcData, PresetBuildActionRequestData info, string sessionID)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ItemEventRouterResponse RemoveWeaponBuild(PmcData pmcData, PresetBuildActionRequestData info, string sessionID)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ItemEventRouterResponse SaveEquipmentBuild(PmcData pmcData, PresetBuildActionRequestData info, string sessionID)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ItemEventRouterResponse RemoveEquipmentBuild(PmcData pmcData, PresetBuildActionRequestData info, string sessionID)
+    {
+        throw new NotImplementedException();
+    }
 }
--- Core/Callbacks/ProfileCallbacks.cs
+++ Core/Callbacks/ProfileCallbacks.cs
@@ -2,5 +2,53 @@
 
 public class ProfileCallbacks
 {
+    public object OnLoad(string sessionID)
+    {
+        throw new NotImplementedException();
+    }
+
+    public GetBodyResponseData<object> CreateProfile(string url, ProfileCreateRequestData info, string sessionID)
+    {
+        throw new NotImplementedException();
+    }
+
+    public GetBodyResponseData<object> GetProfileData(string url, EmptyRequestData info, string sessionID)
+    {
+        throw new NotImplementedException();
+    }
     
+    public GetBodyResponseData<object> RegenerateScav(string url, EmptyRequestData info, string sessionID)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public NullResponseData ChangeVoice(string url, ProfileChangeVoiceRequestData info, string sessionID)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public GetBodyResponseData<object> ChangeNickname(string url, ProfileChangeNicknameRequestData info, string sessionID)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public GetBodyResponseData<object> ValidateNickname(string url, ValidateNicknameRequestData info, string sessionID)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public GetBodyResponseData<string> GetReservedNickname(string url, EmptyRequestData info, string sessionID)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public GetBodyResponseData<object> GetProfileStatus(string url, EmptyRequestData info, string sessionID)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public GetBodyResponseData<SearchFriendResponse> SearchFriend(string url, SearchFriendRequestData info, string sessionID)
+    {
+        throw new NotImplementedException();
+    }
 }
