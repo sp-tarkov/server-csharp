c30dcce1dcad516f431a7452a2468a97f9cd2640
CWX <cwxdev@outlook.com>
2025-01-06 20:17:19 +0000

    Add Globals and XY

---
+++ Core/Models/Eft/Common/XY.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Core.Models.Eft.Common;
+
+public class XY
+{
+    [JsonPropertyName("x")]
+    public double X { get; set; }
+    
+    [JsonPropertyName("y")]
+    public double Y { get; set; }
+}
