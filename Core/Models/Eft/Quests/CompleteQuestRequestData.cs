d59b83dca53db506f20476d8dff4815774e126ad
Cj <161484149+CJ-SPT@users.noreply.github.com>
2025-01-07 06:28:25 -0500

    add quest stuff

---
+++ Core/Models/Eft/Quests/CompleteQuestRequestData.cs
@@ -0,0 +1,16 @@
+using System.Text.Json.Serialization;
+
+namespace Core.Models.Eft.Quests;
+
+public class CompleteQuestRequestData
+{
+	[JsonPropertyName("Action")]
+	public string Action { get; set; }
+
+	/** Quest Id */
+	[JsonPropertyName("qid")]
+	public string QuestId { get; set; }
+
+	[JsonPropertyName("removeExcessItems")]
+	public bool RemoveExcessItems { get; set; }
+}
