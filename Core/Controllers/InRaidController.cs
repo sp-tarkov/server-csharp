a0648b1e8a9dbc66e0aa84c42e7b3ea4583e8b51
Cj <161484149+CJ-SPT@users.noreply.github.com>
2025-01-06 22:32:42 -0500

    More prototypes

--- Core/Controllers/InRaidController.cs
+++ Core/Controllers/InRaidController.cs
@@ -2,5 +2,63 @@ namespace Core.Controllers;
 
 public class InRaidController
 {
-	// TODO
+	/// <summary>
+	/// Save locationId to active profiles in-raid object AND app context
+	/// </summary>
+	/// <param name="sessionId">Session id</param>
+	/// <param name="info">Register player request</param>
+	public void AddPlayer(
+		string sessionId,
+		RegisterPlayerRequestData info)
+	{
+		throw new NotImplementedException();
+	}
+
+	/// <summary>
+	/// Handle raid/profile/scavsave
+	/// Save profile state to disk
+	/// Handles pmc/pscav
+	/// </summary>
+	/// <param name="offRaidProfileData"></param>
+	/// <param name="sessionId"></param>
+	public void SavePostRaidProfileForScav(
+		ScavSaveRequestData offRaidProfileData,
+		string sessionId)
+	{
+		throw new NotImplementedException();
+	}
+
+	/// <summary>
+	/// Get the inraid config from configs/inraid.json
+	/// </summary>
+	public void GetInRaidConfig()
+	{
+		throw new NotImplementedException();
+	}
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="url"></param>
+	/// <param name="sessionId"></param>
+	/// <returns></returns>
+	public float GetTraitorScavHostileChance(
+		string url,
+		string sessionId)
+	{
+		throw new NotImplementedException();
+	}
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="url"></param>
+	/// <param name="sessionId"></param>
+	/// <returns></returns>
+	public List<string> GetBossConvertSettings(
+		string url,
+		string sessionId)
+	{
+		throw new NotImplementedException();
+	}
 }
--- Core/Controllers/LocationController.cs
+++ Core/Controllers/LocationController.cs
@@ -2,5 +2,24 @@ namespace Core.Controllers;
 
 public class LocationController
 {
-	// TODO
+	/// <summary>
+	/// Handle client/locations
+	/// Get all maps base location properties without loot data
+	/// </summary>
+	/// <param name="sessionId">Players Id</param>
+	/// <returns>LocationsGenerateAllResponse</returns>
+	public LocationsGenerateAllResponse GenerateAll(string sessionId)
+	{
+		throw new NotImplementedException();
+	}
+
+	/// <summary>
+	/// Handle client/airdrop/loot
+	/// </summary>
+	/// <param name="request"></param>
+	/// <returns></returns>
+	public GetAirDropLootResponse GetAirDropLoot(GetAirDropLootRequest request)
+	{
+		throw new NotImplementedException();
+	}
 }
