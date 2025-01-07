e80ff158ee14baf2a0155cf0a90d35e6ac7ca5ad
Cj <161484149+CJ-SPT@users.noreply.github.com>
2025-01-07 01:41:07 -0500

    Repair controller

--- Core/Controllers/RepairController.cs
+++ Core/Controllers/RepairController.cs
@@ -1,6 +1,39 @@
+using Core.Models.Eft.Common;
+
 namespace Core.Controllers;
 
 public class RepairController
 {
-	// TODO
+	/// <summary>
+	/// Handle TraderRepair event
+	/// Repair with trader
+	/// </summary>
+	/// <param name="sessionId">session id</param>
+	/// <param name="body">endpoint request data</param>
+	/// <param name="pmcData">player profile</param>
+	/// <returns>item event router action</returns>
+	public ItemEventRouterResponse TraderRepair(
+		string sessionId,
+		TraderRepairActionDataRequest body,
+		PmcData pmcData)
+	{
+		throw new NotImplementedException();
+	}
+	
+	/// <summary>
+	/// Handle Repair event
+	/// Repair with repair kit
+	/// </summary>
+	/// <param name="sessionId">session id</param>
+	/// <param name="body">endpoint request data</param>
+	/// <param name="pmcData">player profile</param>
+	/// <returns></returns>
+	/// <exception cref="NotImplementedException"></exception>
+	public ItemEventRouterResponse RepairWithKit(
+		string sessionId,
+		RepairActionDataRequest body,
+		PmcData pmcData)
+	{
+		throw new NotImplementedException();
+	}
 }
