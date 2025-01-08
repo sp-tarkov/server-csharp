27da4b274caa3892e31f866a166d1f1a80f2d506
CWX <cwxdev@outlook.com>
2025-01-08 13:19:24 +0000

    more nullable

--- Core/Models/Eft/Hideout/QteData.cs
+++ Core/Models/Eft/Hideout/QteData.cs
@@ -5,25 +5,25 @@ using Core.Models.Enums.Hideout;
 
 namespace Core.Models.Eft.Hideout;
 
-public class QteData 
+public class QteData
 {
     [JsonPropertyName("id")]
-    public string Id { get; set; }
+    public string? Id { get; set; }
 
     [JsonPropertyName("type")]
-    public QteActivityType Type { get; set; }
+    public QteActivityType? Type { get; set; }
 
     [JsonPropertyName("area")]
-    public HideoutAreas Area { get; set; }
+    public HideoutAreas? Area { get; set; }
 
     [JsonPropertyName("areaLevel")]
-    public int AreaLevel { get; set; }
+    public int? AreaLevel { get; set; }
 
     [JsonPropertyName("quickTimeEvents")]
-    public List<QuickTimeEvent> QuickTimeEvents { get; set; }
+    public List<QuickTimeEvent>? QuickTimeEvents { get; set; }
 
     [JsonPropertyName("requirements")]
-    public List<object> Requirements { get; set; } 
+    public List<object>? Requirements { get; set; }
     /*
     TODO: Could be an array of any of these:
         | IAreaRequirement
@@ -39,218 +39,219 @@ public class QteData
      */
 
     [JsonPropertyName("results")]
-    public Dictionary<QteEffectType, QteResult> Results { get; set; }
+    public Dictionary<QteEffectType, QteResult>? Results { get; set; }
 }
 
 public class QuickTimeEvent
 {
     [JsonPropertyName("type")]
     [JsonConverter(typeof(JsonStringEnumConverter))]
-    public QteType EventType { get; set; }
-    
+    public QteType? EventType { get; set; }
+
     [JsonPropertyName("position")]
-    public Position Coordinates { get; set; }
-    
+    public Position? Coordinates { get; set; }
+
     [JsonPropertyName("startDelay")]
-    public double StartDelay { get; set; }
-    
+    public double? StartDelay { get; set; }
+
     [JsonPropertyName("endDelay")]
-    public double EndDelay { get; set; }
-    
+    public double? EndDelay { get; set; }
+
     [JsonPropertyName("speed")]
-    public float MovementSpeed { get; set; }
-    
+    public float? MovementSpeed { get; set; }
+
     [JsonPropertyName("successRange")]
-    public Position SuccessCoordinates { get; set; }
-    
+    public Position? SuccessCoordinates { get; set; }
+
     [JsonPropertyName("key")]
-    public string UniqueKey { get; set; }
+    public string? UniqueKey { get; set; }
 }
 
 public class QteRequirement
 {
     [JsonPropertyName("type")]
-    public RequirementType RequirementType { get; set; }
+    public RequirementType? RequirementType { get; set; }
 }
 
 public class QteResult
 {
     [JsonPropertyName("energy")]
-    public int Energy { get; set; }
-    
+    public int? Energy { get; set; }
+
     [JsonPropertyName("hydration")]
-    public int Hydration { get; set; }
-    
+    public int? Hydration { get; set; }
+
     [JsonPropertyName("rewardsRange")]
-    public List<QteEffect> RewardEffects { get; set; }
+    public List<QteEffect>? RewardEffects { get; set; }
 }
 
 public class QteEffect
 {
     [JsonPropertyName("type")]
     [JsonConverter(typeof(JsonStringEnumConverter))]
-    public QteRewardType EffectType { get; set; }
-    
+    public QteRewardType? EffectType { get; set; }
+
     [JsonPropertyName("skillId")]
     [JsonConverter(typeof(JsonStringEnumConverter))]
-    public SkillTypes SkillIdentifier { get; set; }
-    
+    public SkillTypes? SkillIdentifier { get; set; }
+
     [JsonPropertyName("levelMultipliers")]
-    public List<SkillLevelMultiplier> LevelMultipliers { get; set; }
-    
+    public List<SkillLevelMultiplier>? LevelMultipliers { get; set; }
+
     [JsonPropertyName("time")]
-    public int DurationInMilliseconds { get; set; }
-    
+    public int? DurationInMilliseconds { get; set; }
+
     [JsonPropertyName("weight")]
-    public float EffectWeight { get; set; }
-    
+    public float? EffectWeight { get; set; }
+
     [JsonPropertyName("result")]
     [JsonConverter(typeof(JsonStringEnumConverter))]
-    public QteResultType ResultType { get; set; }
+    public QteResultType? ResultType { get; set; }
 }
 
 public class SkillLevelMultiplier
 {
     [JsonPropertyName("level")]
-    public int Level { get; set; }
-    
+    public int? Level { get; set; }
+
     [JsonPropertyName("multiplier")]
-    public float MultiplierValue { get; set; }
+    public float? MultiplierValue { get; set; }
 }
 
 public class Position
 {
     [JsonPropertyName("x")]
-    public float X { get; set; }
+    public float? X { get; set; }
+
     [JsonPropertyName("y")]
-    public float Y { get; set; }
+    public float? Y { get; set; }
 }
 
 public class AreaRequirement : QteRequirement
 {
     [JsonPropertyName("type")]
-    public RequirementType Type { get; set; } = RequirementType.Area;
+    public RequirementType? Type { get; set; } = RequirementType.Area;
 
     [JsonPropertyName("areaType")]
-    public HideoutAreas AreaType { get; set; }
+    public HideoutAreas? AreaType { get; set; }
 
     [JsonPropertyName("requiredLevel")]
-    public int RequiredLevel { get; set; }
+    public int? RequiredLevel { get; set; }
 }
 
 public class TraderUnlockRequirement : QteRequirement
 {
     [JsonPropertyName("type")]
-    public RequirementType Type { get; set; } = RequirementType.TraderUnlock;
+    public RequirementType? Type { get; set; } = RequirementType.TraderUnlock;
 
     [JsonPropertyName("traderId")]
-    public string TraderId { get; set; }
+    public string? TraderId { get; set; }
 }
 
 public class TraderLoyaltyRequirement : QteRequirement
 {
     [JsonPropertyName("type")]
-    public RequirementType Type { get; set; } = RequirementType.TraderLoyalty;
+    public RequirementType? Type { get; set; } = RequirementType.TraderLoyalty;
 
     [JsonPropertyName("traderId")]
-    public string TraderId { get; set; }
+    public string? TraderId { get; set; }
 
     [JsonPropertyName("loyaltyLevel")]
-    public int LoyaltyLevel { get; set; }
+    public int? LoyaltyLevel { get; set; }
 }
 
 public class SkillRequirement : QteRequirement
 {
     [JsonPropertyName("type")]
-    public RequirementType Type { get; set; } = RequirementType.Skill;
+    public RequirementType? Type { get; set; } = RequirementType.Skill;
 
     [JsonPropertyName("skillName")]
-    public SkillTypes SkillName { get; set; }
+    public SkillTypes? SkillName { get; set; }
 
     [JsonPropertyName("skillLevel")]
-    public int SkillLevel { get; set; }
+    public int? SkillLevel { get; set; }
 }
 
 public class ResourceRequirement : QteRequirement
 {
     [JsonPropertyName("type")]
-    public RequirementType Type { get; set; } = RequirementType.Resource;
+    public RequirementType? Type { get; set; } = RequirementType.Resource;
 
     [JsonPropertyName("templateId")]
-    public string TemplateId { get; set; }
+    public string? TemplateId { get; set; }
 
     [JsonPropertyName("resource")]
-    public int Resource { get; set; }
+    public int? Resource { get; set; }
 }
 
 public class ItemRequirement : QteRequirement
 {
     [JsonPropertyName("type")]
-    public RequirementType Type { get; set; } = RequirementType.Item;
+    public RequirementType? Type { get; set; } = RequirementType.Item;
 
     [JsonPropertyName("templateId")]
-    public string TemplateId { get; set; }
+    public string? TemplateId { get; set; }
 
     [JsonPropertyName("count")]
-    public int Count { get; set; }
+    public int? Count { get; set; }
 
     [JsonPropertyName("isFunctional")]
-    public bool IsFunctional { get; set; }
+    public bool? IsFunctional { get; set; }
 
     [JsonPropertyName("isEncoded")]
-    public bool IsEncoded { get; set; }
+    public bool? IsEncoded { get; set; }
 }
 
 public class ToolRequirement : QteRequirement
 {
     [JsonPropertyName("type")]
-    public RequirementType Type { get; set; } = RequirementType.Tool;
+    public RequirementType? Type { get; set; } = RequirementType.Tool;
 
     [JsonPropertyName("templateId")]
-    public string TemplateId { get; set; }
+    public string? TemplateId { get; set; }
 
     [JsonPropertyName("count")]
-    public int Count { get; set; }
+    public int? Count { get; set; }
 
     [JsonPropertyName("isFunctional")]
-    public bool IsFunctional { get; set; }
+    public bool? IsFunctional { get; set; }
 
     [JsonPropertyName("isEncoded")]
-    public bool IsEncoded { get; set; }
+    public bool? IsEncoded { get; set; }
 }
 
 public class QuestRequirement : QteRequirement
 {
     [JsonPropertyName("type")]
-    public RequirementType Type { get; set; } = RequirementType.QuestComplete;
+    public RequirementType? Type { get; set; } = RequirementType.QuestComplete;
 
     [JsonPropertyName("questId")]
-    public string QuestId { get; set; }
+    public string? QuestId { get; set; }
 }
 
 public class HealthRequirement : QteRequirement
 {
     [JsonPropertyName("type")]
-    public RequirementType Type { get; set; } = RequirementType.Health;
+    public RequirementType? Type { get; set; } = RequirementType.Health;
 
     [JsonPropertyName("energy")]
-    public int Energy { get; set; }
+    public int? Energy { get; set; }
 
     [JsonPropertyName("hydration")]
-    public int Hydration { get; set; }
+    public int? Hydration { get; set; }
 }
 
 public class BodyPartBuffRequirement : QteRequirement
 {
     [JsonPropertyName("type")]
-    public RequirementType Type { get; set; } = RequirementType.BodyPartBuff;
+    public RequirementType? Type { get; set; } = RequirementType.BodyPartBuff;
 
     [JsonPropertyName("effectName")]
-    public Effect EffectName { get; set; }
+    public Effect? EffectName { get; set; }
 
     [JsonPropertyName("bodyPart")]
-    public BodyPart BodyPart { get; set; }
+    public BodyPart? BodyPart { get; set; }
 
     [JsonPropertyName("excluded")]
-    public bool Excluded { get; set; }
+    public bool? Excluded { get; set; }
 }
