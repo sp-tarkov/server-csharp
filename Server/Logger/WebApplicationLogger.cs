e7d328b75c44eeb1ac84875a4ab0c78aa0a117aa
Alex <alex@dm-me-for-questions.com>
2025-01-13 20:16:05 +0000

    added color logging

--- Server/Logger/WebApplicationLogger.cs
+++ Server/Logger/WebApplicationLogger.cs
@@ -1,4 +1,5 @@
 using Core.Annotations;
+using Core.Models.Logging;
 using ILogger = Core.Models.Utils.ILogger;
 
 namespace Server.Logger;
@@ -12,19 +13,43 @@ public class WebApplicationLogger : ILogger
         _logger = provider.CreateLogger("SptLogger");
     }
 
+    public void LogWithColor(string data, LogTextColor? textColor = null, LogBackgroundColor? backgroundColor = null)
+    {
+        if (textColor != null || backgroundColor != null)
+        {
+            _logger.LogInformation(GetColorizedText(data, textColor, backgroundColor));
+        }
+        else 
+            _logger.LogInformation(data);
+    }
+
+    private string GetColorizedText(string data, LogTextColor? textColor = null, LogBackgroundColor? backgroundColor = null)
+    {
+        var colorString = string.Empty;
+        if (textColor != null)
+            colorString += ((int)textColor.Value).ToString();
+            
+        if (backgroundColor != null)
+            colorString += string.IsNullOrEmpty(colorString)
+                ? ((int)backgroundColor.Value).ToString()
+                : $";{((int)backgroundColor.Value).ToString()}";
+        
+        return $"\x1b[{colorString}m{data}\x1b[0m";
+    }
+
     public void Success(string data)
     {
-        _logger.LogInformation(data);
+        _logger.LogInformation(GetColorizedText(data, LogTextColor.Green));
     }
 
     public void Error(string data)
     {
-        _logger.LogError(data);
+        _logger.LogError(GetColorizedText(data, LogTextColor.Red));
     }
 
     public void Warning(string data)
     {
-        _logger.LogWarning(data);
+        _logger.LogWarning(GetColorizedText(data, LogTextColor.Yellow));
     }
     
     public void Info(string data)
@@ -35,5 +60,10 @@ public class WebApplicationLogger : ILogger
     public void Debug(string data)
     {
         _logger.LogDebug(data);
+    }
+
+    public void Critical(string data)
+    {
+        _logger.LogCritical(GetColorizedText(data, LogTextColor.Black, LogBackgroundColor.Red));
     }
 }
