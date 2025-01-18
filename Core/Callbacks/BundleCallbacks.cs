7b6068e73a3a0686fd2c10a0e0e2c2bb5dedad05
CWX <cwxdev@outlook.com>
2025-01-18 12:46:54 +0000

    use prim ctor and fix warnings

--- Core/Callbacks/BundleCallbacks.cs
+++ Core/Callbacks/BundleCallbacks.cs
@@ -1,33 +1,15 @@
 ﻿using Core.Annotations;
 using Core.Models.Eft.Common;
-using Core.Models.Enums;
-using Core.Models.Spt.Config;
-using Core.Servers;
 using Core.Utils;
 
 namespace Core.Callbacks;
 
 [Injectable(InjectableTypeOverride = typeof(BundleCallbacks))]
-public class BundleCallbacks
+public class BundleCallbacks(
+    HttpResponseUtil _httpResponseUtil
+    // BundleLoader _bundleLoader,
+)
 {
-    protected HttpResponseUtil _httpResponseUtil;
-    // protected BundleLoader _bundleLoader; TODO: this needs implementing
-    protected ConfigServer _configServer;
-    protected HttpConfig _httpConfig;
-
-    public BundleCallbacks
-    (
-        HttpResponseUtil httpResponseUtil,
-        // BundleLoader bundleLoader,
-        ConfigServer configServer
-    )
-    {
-        _httpResponseUtil = httpResponseUtil;
-        // _bundleLoader = bundleLoader;
-        _configServer = configServer;
-        _httpConfig = configServer.GetConfig<HttpConfig>();
-    }
-
     /// <summary>
     /// Handle singleplayer/bundles
     /// </summary>
