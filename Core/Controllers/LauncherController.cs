5931cace0a68e92991cd4efc938259b02519676c
CWX <cwxdev@outlook.com>
2025-01-19 12:59:25 +0000

    fix launcher controller warnings

--- Core/Controllers/LauncherController.cs
+++ Core/Controllers/LauncherController.cs
@@ -67,7 +67,7 @@ public class LauncherController(
             }
 
             var casterPropertyValue = propertyValue as ProfileSides;
-            result[templatesProperty.GetJsonName()] = _localisationService.GetText(casterPropertyValue.DescriptionLocaleKey);
+            result[templatesProperty.GetJsonName()] = _localisationService.GetText(casterPropertyValue?.DescriptionLocaleKey!);
         }
 
         return result;
@@ -96,7 +96,7 @@ public class LauncherController(
     {
         foreach (var kvp in _saveServer.GetProfiles())
         {
-            if (info.Username == _saveServer.GetProfile(kvp.Key).ProfileInfo.Username)
+            if (info.Username == _saveServer.GetProfile(kvp.Key).ProfileInfo?.Username)
             {
                 return "";
             }
@@ -142,25 +142,25 @@ public class LauncherController(
         return timeStampStr.ToLower() + counterStr.ToLower();
     }
 
-    public string ChangeUsername(ChangeRequestData info)
+    public string? ChangeUsername(ChangeRequestData info)
     {
         var sessionID = Login(info);
 
         if (!string.IsNullOrEmpty(sessionID))
         {
-            _saveServer.GetProfile(sessionID).ProfileInfo.Username = info.Change;
+            _saveServer.GetProfile(sessionID).ProfileInfo!.Username = info.Change;
         }
 
         return sessionID;
     }
 
-    public string ChangePassword(ChangeRequestData info)
+    public string? ChangePassword(ChangeRequestData info)
     {
         var sessionID = Login(info);
 
         if (!string.IsNullOrEmpty(sessionID))
         {
-            _saveServer.GetProfile(sessionID).ProfileInfo.Password = info.Change;
+            _saveServer.GetProfile(sessionID).ProfileInfo!.Password = info.Change;
         }
 
         return sessionID;
@@ -183,7 +183,7 @@ public class LauncherController(
         if (!string.IsNullOrEmpty(sessionID))
         {
             var profileInfo = _saveServer.GetProfile(sessionID).ProfileInfo;
-            profileInfo.Edition = info.Edition;
+            profileInfo!.Edition = info.Edition;
             profileInfo.IsWiped = true;
         }
 
@@ -213,7 +213,7 @@ public class LauncherController(
      */
     public List<ModDetails> GetServerModsProfileUsed(string sessionId)
     {
-        var profile = _profileHelper.GetFullProfile(sessionId);
+        // var profile = _profileHelper.GetFullProfile(sessionId);
 
         _logger.Error("NOT IMPLEMENTED - _preSptModLoader GetServerModsProfileUsed()");
         /* TODO => modding
