32d9830fe962096426e9a73b35fe885e08db6290
Chomp <dev@dev.sp-tarkov.com>
2025-01-23 15:58:29 +0000

    Added missing type

---
+++ Libraries/Core/Models/Common/IdWithCount.cs
@@ -0,0 +1,14 @@
+using System.Text.Json.Serialization;
+
+namespace Core.Models.Common;
+
+public record IdWithCount
+{
+    /** Id of stack to take money from */
+    [JsonPropertyName("id")]
+    public string? Id { get; set; }
+
+    /** Amount of money to take off player for treatment */
+    [JsonPropertyName("count")]
+    public double? Count { get; set; }
+}
