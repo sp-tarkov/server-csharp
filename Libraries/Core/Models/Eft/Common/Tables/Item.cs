60ea5d074a70d9dfd4dfb52c3687d52d42295ed5
CWX <cwxdev@outlook.com>
2025-01-23 13:37:48 +0000

    add missing jsonPropNames

--- Libraries/Core/Models/Eft/Common/Tables/Item.cs
+++ Libraries/Core/Models/Eft/Common/Tables/Item.cs
@@ -43,12 +43,17 @@ public record Item
 
 public record ItemLocation
 {
+    [JsonPropertyName("x")]
     public int? X { get; set; }
+    [JsonPropertyName("y")]
     public int? Y { get; set; }
+    [JsonPropertyName("r")]
     public object? R { get; set; } // TODO: Can be string or number
+    [JsonPropertyName("isSearched")]
     public bool? IsSearched { get; set; }
 
     /** SPT property? */
+    [JsonPropertyName("rotation")]
     public object? Rotation { get; set; } // TODO: Can be string or boolean
 }
 
