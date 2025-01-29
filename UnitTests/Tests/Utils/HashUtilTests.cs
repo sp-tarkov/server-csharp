ff47b99cf54c8ac9cb7b3b4cb55de44e31835fa0
Chomp <dev@dev.sp-tarkov.com>
2025-01-29 13:30:31 +0000

    Improved `IsValidMongoIdTest`

--- UnitTests/Tests/Utils/HashUtilTests.cs
+++ UnitTests/Tests/Utils/HashUtilTests.cs
@@ -29,30 +29,15 @@ public class HashUtilTests
 	}
 	
 	[TestMethod]
-	public void IsValidMongoIdTest()
+    [DataRow("677ddb67406e9918a0264bbz", false, "677ddb67406e9918a0264bbz contains invalid char `z`, but result was true")]
+    [DataRow("677ddb67406e9918a0264bbcc", false, "677ddb67406e9918a0264bbcc is 25 characters, but result was true")]
+    [DataRow("677ddb67406e9918a0264bbc", true, "IsValidMongoId() `677ddb67406e9918a0264bbc` is a valid mongoId, but result was false")]
+public void IsValidMongoIdTest(string mongoId, bool passes, string failMessage)
 	{
-		// Invalid mongoId character
-		var ResultBadChar = _hashUtil.IsValidMongoId("677ddb67406e9918a0264bbz");
-		
-		Assert.AreEqual(
-			false, 
-			ResultBadChar,
-			"IsValidMongoId() `677ddb67406e9918a0264bbz` contains invalid char `z`, but result was true");
-		
-		// Invalid mongoId length
-		var resultBadLength = _hashUtil.IsValidMongoId("677ddb67406e9918a0264bbcc");
-		
-		Assert.AreEqual(
-			false, 
-			resultBadLength,
-			"IsValidMongoId() `677ddb67406e9918a0264bbcc` is 25 characters, but result was true");
-		
-		// Valid mongoId
-		var resultPass = _hashUtil.IsValidMongoId("677ddb67406e9918a0264bbc");
-		
-		Assert.AreEqual(
-			true, 
-			resultPass,
-			"IsValidMongoId() `677ddb67406e9918a0264bbc` is a valid mongoId, but result was false");
+        var result = _hashUtil.IsValidMongoId(mongoId);
+        Assert.AreEqual(
+            passes,
+            result,
+            failMessage);
 	}
 }
