2e9f2f2914eefd0558e0becb078f4f984d34a5b5
Chomp <dev@dev.sp-tarkov.com>
2025-01-30 14:16:59 +0000

    Updated `TraderAssortService` to be readonly

--- Libraries/Core/Services/TraderAssortService.cs
+++ Libraries/Core/Services/TraderAssortService.cs
@@ -1,4 +1,4 @@
-﻿using SptCommon.Annotations;
+using SptCommon.Annotations;
 using Core.Models.Eft.Common.Tables;
 
 namespace Core.Services;
@@ -6,7 +6,7 @@ namespace Core.Services;
 [Injectable(InjectionType.Singleton)]
 public class TraderAssortService
 {
-    private Dictionary<string, TraderAssort> _pristineTraderAssorts = new();
+    protected readonly Dictionary<string, TraderAssort> _pristineTraderAssorts = new();
     
     public TraderAssort? GetPristineTraderAssort(string traderId)
     {
@@ -18,7 +18,7 @@ public class TraderAssortService
     /// <summary>
     /// Store trader assorts inside a class property
     /// </summary>
-    /// <param name="traderId">Traderid to store assorts against</param>
+    /// <param name="traderId">Trader id to store assorts against</param>
     /// <param name="assort">Assorts to store</param>
     public void SetPristineTraderAssort(string traderId, TraderAssort assort)
     {
