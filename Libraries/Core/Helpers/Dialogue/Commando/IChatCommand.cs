4d0d3c65964d585eeb678a4e0b22110b94557cbb
CWX <cwxdev@outlook.com>
2025-02-10 21:24:17 +0000

    finish off example 21, updated temp dll's with fixes

--- Libraries/Core/Helpers/Dialogue/Commando/IChatCommand.cs
+++ Libraries/Core/Helpers/Dialogue/Commando/IChatCommand.cs
@@ -8,5 +8,5 @@ public interface IChatCommand
     public string GetCommandPrefix();
     public string GetCommandHelp(string command);
     public List<string> GetCommands();
-    public string Handle(string command, UserDialogInfo commandHandler, string sesssionId, SendMessageRequest request);
+    public string Handle(string command, UserDialogInfo commandHandler, string sessionId, SendMessageRequest request);
 }
