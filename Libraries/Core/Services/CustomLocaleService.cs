1abdaaed4f8c824302e838f3ac25103ad8d44428
Chomp <dev@dev.sp-tarkov.com>
2025-02-12 21:56:59 +0000

    Fixed typo

--- Libraries/Core/Services/CustomLocaleService.cs
+++ Libraries/Core/Services/CustomLocaleService.cs
@@ -2,36 +2,35 @@
 using Core.Utils;
 using SptCommon.Annotations;
 
-namespace Core.Services
+namespace Core.Services;
+
+[Injectable]
+public class CustomLocaleService(
+    ISptLogger<CustomLocaleService> logger,
+    FileUtil fileUtil,
+    DatabaseService databaseService
+)
 {
-    [Injectable]
-    public class CustomLocaleService(
-        ISptLogger<CustomLocaleService> logger,
-        FileUtil fileUtil,
-        DatabaseService databaseService
-        )
+
+    /// <summary>
+    /// Path should link to a folder containing every locale that should be added to the server locales
+    /// e.g. en.json for english, fr.json for french
+    /// Inside each JSON should be a Dictionary of the locale key and localised text
+    /// </summary>
+    /// <param name="pathToServerLocales">A path to a folder that contains locales to add to SPT</param>
+    public void AddServerLocales(string pathToServerLocales)
     {
 
-        /// <summary>
-        /// Path should link to a folder containing every locale that should be added to the server locales
-        /// e.g. en.json for english, fr.json for french
-        /// Inside each JSON should be a Dictionary of the locale key and localised text
-        /// </summary>
-        /// <param name="pathToServerLocales">A path to a folder that contains locales to add to SPT</param>
-        public void AdServerLocales(string pathToServerLocales)
-        {
+    }
 
-        }
+    /// <summary>
+    /// Path should link to a folder containing every locale that should be added to the game locales
+    /// e.g. en.json for english, fr.json for french
+    /// Inside each JSON should be a Dictionary of the locale key and localised text
+    /// </summary>
+    /// <param name="pathToGameLocales">A path to a folder that contains locales to add to SPT</param>
+    public void AddGameLocales(string pathToGameLocales)
+    {
 
-        /// <summary>
-        /// Path should link to a folder containing every locale that should be added to the game locales
-        /// e.g. en.json for english, fr.json for french
-        /// Inside each JSON should be a Dictionary of the locale key and localised text
-        /// </summary>
-        /// <param name="pathToGameLocales">A path to a folder that contains locales to add to SPT</param>
-        public void AddGameLocales(string pathToGameLocales)
-        {
-
-        }
     }
 }
