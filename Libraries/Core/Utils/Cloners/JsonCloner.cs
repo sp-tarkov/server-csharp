e9d42e3b32224edf51e07cfedf82a716c9d6edd3
Archangel <jesse@archangel.wtf>
2025-02-14 14:44:20 +0100

    Revert "Clone using a MemoryStream"
    This reverts commit 76ade326f510cc652b8d36d331e8af4c47ca4037.

--- Libraries/Core/Utils/Cloners/JsonCloner.cs
+++ Libraries/Core/Utils/Cloners/JsonCloner.cs
@@ -1,4 +1,3 @@
-using System.Text.Json;
 using SptCommon.Annotations;
 
 namespace Core.Utils.Cloners;
@@ -6,14 +5,15 @@ namespace Core.Utils.Cloners;
 [Injectable]
 public class JsonCloner : ICloner
 {
+    protected JsonUtil _jsonUtil;
+
+    public JsonCloner(JsonUtil jsonUtil)
+    {
+        _jsonUtil = jsonUtil;
+    }
+
     public T? Clone<T>(T? obj)
     {
-        using (MemoryStream ms = new())
-        {
-            JsonSerializer.Serialize(ms, obj);
-            ms.Seek(0, SeekOrigin.Begin);
-
-            return JsonSerializer.Deserialize<T>(ms);
-        }
+        return _jsonUtil.Deserialize<T>(_jsonUtil.Serialize(obj));
     }
 }
