37d61338e65f58192f566c1c319c5e1f29479f9f
Chomp <dev@dev.sp-tarkov.com>
2025-03-11 20:30:41 +0000

    Improved comment for `CalculateLevel`

--- Libraries/SPTarkov.Server.Core/Services/PlayerService.cs
+++ Libraries/SPTarkov.Server.Core/Services/PlayerService.cs
@@ -9,10 +9,13 @@ public class PlayerService(
 )
 {
     /// <summary>
-    /// Get level of player
+    /// Calculates the current level of a player based on their accumulated experience points.
+    /// This method iterates through an experience table to determine the highest level achieved
+    /// by comparing the player's experience against cumulative thresholds.
     /// </summary>
     /// <param name="pmcData"> Player profile </param>
-    /// <returns> Level of the player </returns>
+    /// <returns> The calculated level of the player as an integer, or null if the level cannot be determined.
+    /// This value is also assigned to <see cref="PmcData.Info.Level"/> within the provided profile. </returns>
     public int? CalculateLevel(PmcData pmcData)
     {
         var accExp = 0;
