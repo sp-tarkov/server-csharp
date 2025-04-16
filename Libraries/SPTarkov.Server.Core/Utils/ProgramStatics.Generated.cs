48775a39ed524820dce5cff5eaf2ceb29cd014b9
CWX <cwxdev@outlook.com>
2025-04-16 10:59:43 +0100

    make static props private

--- Libraries/SPTarkov.Server.Core/Utils/ProgramStatics.Generated.cs
+++ Libraries/SPTarkov.Server.Core/Utils/ProgramStatics.Generated.cs
@@ -4,8 +4,8 @@ namespace SPTarkov.Server.Core.Utils;
 
 public static partial class ProgramStatics
 {
-    public static string? _sptVersion = "0.0.0";
-    public static string? _commit = "a12b34";
-    public static double? _buildTime = 0000000000;
-    public static EntryType? BuildType = EntryType.LOCAL;
+    private static string? _sptVersion = "0.0.0";
+    private static string? _commit = "a12b34";
+    private static double? _buildTime = 0000000000;
+    private static EntryType? BuildType = EntryType.LOCAL;
 }
