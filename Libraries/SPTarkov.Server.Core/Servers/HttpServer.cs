29a7865a52b0bc099cd4df5504ab06ab0f4e0434
hulkhan22 <clandestine984@gmail.com>
2025-05-01 09:18:10 +0200

    fix: Infinite loading screen (#216)
    * Fix various issues that cause infinite loading
    
    * Revert nullable

--- Libraries/SPTarkov.Server.Core/Servers/HttpServer.cs
+++ Libraries/SPTarkov.Server.Core/Servers/HttpServer.cs
@@ -88,7 +88,16 @@ public class HttpServer(
         context.Request.Cookies.TryGetValue("PHPSESSID", out var sessionId);
         if (sessionId != null)
         {
-            _applicationContext.AddValue(ContextVariableType.SESSION_ID, sessionId);
+            try
+            {
+                _applicationContext.AddValue(ContextVariableType.SESSION_ID, sessionId);
+            }
+            catch (Exception ex)
+            {
+                _logger.Debug("Error while adding context value: " + ex.Message);
+                _logger.Critical(ex.StackTrace);
+                throw;
+            }
         }
 
         // Extract header for original IP detection
@@ -106,6 +115,7 @@ public class HttpServer(
         }
         catch (Exception ex)
         {
+            _logger.Debug("Error handling request: " + context.Request.Path);
             _logger.Critical(ex.Message);
             _logger.Critical(ex.StackTrace);
         }
