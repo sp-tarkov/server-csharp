3b22192160f510b75dc9fadf74842777c236ddef
Alex <clodanSPT@hotmail.com>
2025-05-28 10:51:06 +0100

    Removed ConcurrentDictionary in favor of locked dictionary, and added reconnection behaviour to OnConnection

--- Libraries/SPTarkov.Server.Core/Servers/Ws/SptWebSocketConnectionHandler.cs
+++ Libraries/SPTarkov.Server.Core/Servers/Ws/SptWebSocketConnectionHandler.cs
@@ -1,4 +1,3 @@
-using System.Collections.Concurrent;
 using System.Net.WebSockets;
 using System.Text;
 using SPTarkov.DI.Annotations;
@@ -18,12 +17,11 @@ public class SptWebSocketConnectionHandler(
     LocalisationService _localisationService,
     JsonUtil _jsonUtil,
     ProfileHelper _profileHelper,
-    ConfigServer _configServer,
     IEnumerable<ISptWebSocketMessageHandler> _messageHandlers
 ) : IWebSocketConnectionHandler
 {
-    protected WsPing _defaultNotification = new();
-    protected ConcurrentDictionary<string, WebSocket> _sockets = new();
+    protected Dictionary<string, WebSocket> _sockets = new();
+    protected object _socketsLock = new();
 
     public string GetHookUrl()
     {
@@ -41,23 +39,34 @@ public class SptWebSocketConnectionHandler(
         var sessionID = splitUrl.Last();
         var playerProfile = _profileHelper.GetFullProfile(sessionID);
         var playerInfoText = $"{playerProfile.ProfileInfo.Username} ({sessionID})";
-
-        _logger.Info(_localisationService.GetText("websocket-player_connected", playerInfoText));
-
-        if (!_sockets.TryAdd(sessionID, ws) && _logger.IsLogEnabled(LogLevel.Debug))
+        lock (_socketsLock)
         {
-            _logger.Debug($"[ws] player: {playerInfoText} has already connected");
-        }
+            if (_sockets.TryGetValue(sessionID, out var oldSocket))
+            {
+                if (_logger.IsLogEnabled(LogLevel.Debug))
+                {
+                    _logger.Debug(_localisationService.GetText("websocket-player_reconnect", playerInfoText));
+                }
 
-        return Task.CompletedTask;
+                oldSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, string.Empty, CancellationToken.None).Wait();
+                _sockets.Remove(sessionID);
+            }
+
+            _sockets.Add(sessionID, ws);
+            if (_logger.IsLogEnabled(LogLevel.Info))
+            {
+                _logger.Info(_localisationService.GetText("websocket-player_connected", playerInfoText));
+            }
+
+            return Task.CompletedTask;
+        }
     }
 
-    public async Task OnMessage(byte[] receivedMessage, WebSocketMessageType messageType, WebSocket ws, HttpContext context)
+    public async Task OnMessage(byte[] receivedMessage, WebSocketMessageType messageType, WebSocket ws,
+        HttpContext context)
     {
         var splitUrl = context.Request.Path.Value.Split("/");
         var sessionID = splitUrl.Last();
-        var playerProfile = _profileHelper.GetFullProfile(sessionID);
-        var playerInfoText = $"{playerProfile.ProfileInfo.Username} ({sessionID})";
 
         foreach (var sptWebSocketMessageHandler in _messageHandlers)
         {
@@ -70,14 +79,20 @@ public class SptWebSocketConnectionHandler(
         var splitUrl = context.Request.Path.Value.Split("/");
         var sessionID = splitUrl.Last();
 
-        if (!_sockets.Remove(sessionID, out _) && _logger.IsLogEnabled(LogLevel.Debug))
+        lock (_socketsLock)
         {
-            _logger.Debug($"[ws] Error removing socket for session: {sessionID}");
-        }
+            if (!_sockets.Remove(sessionID, out _) && _logger.IsLogEnabled(LogLevel.Info))
+            {
+                _logger.Info($"[ws] The websocket session {sessionID} has already been removed or reconnected");
+            }
 
-        var playerProfile = _profileHelper.GetFullProfile(sessionID);
-        var playerInfoText = $"{playerProfile.ProfileInfo.Username} ({sessionID})";
-        _logger.Info($"[ws] player: {playerInfoText} has disconnected");
+            var playerProfile = _profileHelper.GetFullProfile(sessionID);
+            var playerInfoText = $"{playerProfile.ProfileInfo.Username} ({sessionID})";
+            if (_logger.IsLogEnabled(LogLevel.Info))
+            {
+                _logger.Info($"[ws] player: {playerInfoText} has disconnected");
+            }
+        }
     }
 
     public void SendMessage(string sessionID, WsNotificationEvent output)
