c374378cb01e0dadc3acd0c4cc76d987c7ade22a
Alex <alex@dm-me-for-questions.com>
2025-05-30 09:04:09 +0100

    OnLoadOrder sort

--- Libraries/SPTarkov.Server.Core/DI/OnLoadOrder.cs
+++ Libraries/SPTarkov.Server.Core/DI/OnLoadOrder.cs
@@ -2,19 +2,15 @@ namespace SPTarkov.Server.Core.DI;
 
 public static class OnLoadOrder
 {
-    public const int PreSPTDatabase = 0;
-    public const int Database = 1;
-    public const int PostSptDatabase = 2;
+    public const int Database = 0;
     public const int GameCallbacks = 100;
     public const int PostDBModLoader = 200;
-    public const int TraderRegistration = 250;
-    public const int HandbookCallbacks = 300;
-    public const int HttpCallbacks = 400;
-    public const int SaveCallbacks = 500;
-    public const int TraderCallbacks = 600;
-    public const int PostSptModLoader = 700;
+    public const int TraderRegistration = 300;
+    public const int HandbookCallbacks = 400;
+    public const int HttpCallbacks = 500;
+    public const int SaveCallbacks = 600;
+    public const int TraderCallbacks = 700;
     public const int PresetCallbacks = 800;
-    public const int RagfairPriceService = 900;
-    public const int RagfairCallbacks = 1000;
-    public const int PostServerLoad = 9999;
+    public const int RagfairCallbacks = 900;
+    public const int PostSptModLoader = 1000;
 }
