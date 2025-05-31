65d47b375a53a59127ac2016e5ab20be60cb2315
Archangel <jesse@archangel.wtf>
2025-05-31 20:06:55 +0200

    Cleanup GetResponse

--- Libraries/SPTarkov.Server.Core/Servers/Http/SptHttpListener.cs
+++ Libraries/SPTarkov.Server.Core/Servers/Http/SptHttpListener.cs
@@ -188,22 +188,13 @@ public class SptHttpListener(
 
     public async ValueTask<string> GetResponse(string sessionID, HttpRequest req, string? body)
     {
-        //Todo: deserializedObject was an out in GetResponse but it was never assigned to anything in GetResponse, is this still necessary?
-        object deserializedObject = new();
         var output = await _router.GetResponse(req, sessionID, body);
+
         /* route doesn't exist or response is not properly set up */
         if (string.IsNullOrEmpty(output))
         {
             _logger.Error(_localisationService.GetText("unhandled_response", req.Path.ToString()));
-            _logger.Info(_jsonUtil.Serialize(deserializedObject));
             output = _httpResponseUtil.GetBody<object?>(null, BackendErrorCodes.HTTPNotFound, $"UNHANDLED RESPONSE: {req.Path.ToString()}");
-        }
-
-        if (ProgramStatics.ENTRY_TYPE() != EntryType.RELEASE)
-        {
-            // Parse quest info into object
-            var log = new Request(req.Method, new RequestData(req.Path, req.Headers, deserializedObject));
-            _requestsLogger.Info($"REQUEST={_jsonUtil.Serialize(log)}");
         }
 
         return output;
