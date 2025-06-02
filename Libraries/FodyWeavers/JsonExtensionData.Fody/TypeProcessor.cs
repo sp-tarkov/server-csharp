66503c30390e58789817a37274990e49e3ee2dea
Alex <clodanSPT@hotmail.com>
2025-06-02 11:41:51 +0100

    retargeted fody plugin to netstandard for msbuild runtime

--- Libraries/FodyWeavers/JsonExtensionData.Fody/TypeProcessor.cs
+++ Libraries/FodyWeavers/JsonExtensionData.Fody/TypeProcessor.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections.Generic;
-using System.Text.Json.Serialization;
-
+using System.Linq;
 using Mono.Cecil;
-using BindingFlags = System.Reflection.BindingFlags;
 
 namespace JsonExtensionData.Fody;
 
@@ -13,7 +11,14 @@ public partial class ModuleWeaver
     public void ProcessType(TypeDefinition typeDefinition)
     {
         _dictionaryStringObjectReference ??= ModuleDefinition.ImportReference(typeof(Dictionary<string, object>));
-        _jsonExtensionDataAttributeReference ??= ModuleDefinition.ImportReference(typeof(JsonExtensionDataAttribute).GetConstructor(BindingFlags.Public | BindingFlags.Instance, []));
+        if (_jsonExtensionDataAttributeReference is null)
+        {
+            var jsonConstructorReference = ModuleDefinition.AssemblyResolver
+                .Resolve(AssemblyNameReference.Parse("System.Text.Json")).MainModule
+                .GetType("System.Text.Json.Serialization.JsonExtensionDataAttribute").Methods
+                .First(m => m.IsConstructor && !m.HasParameters);
+            _jsonExtensionDataAttributeReference = ModuleDefinition.ImportReference(jsonConstructorReference);
+        }
         var propertyDefinition = new PropertyDefinition("ExtensionData", PropertyAttributes.None, _dictionaryStringObjectReference);
         propertyDefinition.CustomAttributes.Add(new CustomAttribute(_jsonExtensionDataAttributeReference));
         var get = new MethodDefinition("get_ExtensionData",
