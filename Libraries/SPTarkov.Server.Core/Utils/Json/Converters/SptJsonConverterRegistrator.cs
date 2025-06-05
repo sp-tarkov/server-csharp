dcfdbf9e0c433cd1af0f1f9839473dc61cf0eb1d
Archangel <jesse@archangel.wtf>
2025-06-06 01:02:52 +0200

    Fix up converters to target all assemblies in the AppDomain

--- Libraries/SPTarkov.Server.Core/Utils/Json/Converters/SptJsonConverterRegistrator.cs
+++ Libraries/SPTarkov.Server.Core/Utils/Json/Converters/SptJsonConverterRegistrator.cs
@@ -23,10 +23,12 @@ public class SptJsonConverterRegistrator : IJsonConverterRegistrator
 
     private static List<JsonConverter> GetGenericJsonConverters()
     {
-        var enums = Assembly.GetExecutingAssembly().GetTypes()
+        var enums = AppDomain.CurrentDomain.GetAssemblies()
+            .SelectMany(assembly => assembly.GetTypes())
             .Where(type => type.IsEnum && type.GetCustomAttribute<EftEnumConverterAttribute>() != null);
 
-        var listEnums = Assembly.GetExecutingAssembly().GetTypes()
+        var listEnums = AppDomain.CurrentDomain.GetAssemblies()
+            .SelectMany(assembly => assembly.GetTypes())
             .Where(type => type.IsEnum && type.GetCustomAttribute<EftListEnumConverterAttribute>() != null);
 
         var result = enums.Select(e => (JsonConverter) Activator.CreateInstance(typeof(EftEnumConverter<>).MakeGenericType(e))!).ToList();
