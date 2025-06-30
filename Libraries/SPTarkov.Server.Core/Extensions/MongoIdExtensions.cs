65d25840c858e1bfe553e562b6a78c0e8f7167c1
Archangel <jesse@archangel.wtf>
2025-06-30 18:13:00 +0200

    Push new extensions

---
+++ Libraries/SPTarkov.Server.Core/Extensions/MongoIdExtensions.cs
@@ -0,0 +1,13 @@
+﻿using SPTarkov.Server.Core.Models.Common;
+
+namespace SPTarkov.Server.Core.Extensions
+{
+    public static class MongoIdExtensions
+    {
+        //Temporary, but necessary
+        public static IEnumerable<MongoId> ToMongoIds(this IEnumerable<string> source)
+        {
+            return source.Select(s => (MongoId)s);
+        }
+    }
+}
