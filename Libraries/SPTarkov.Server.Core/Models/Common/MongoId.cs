99a0a71ef47264f6be7bf43a5233f388efffa7f0
Archangel <jesse@archangel.wtf>
2025-06-30 19:15:21 +0200

    Handle null strings, fixes item events

--- Libraries/SPTarkov.Server.Core/Models/Common/MongoId.cs
+++ Libraries/SPTarkov.Server.Core/Models/Common/MongoId.cs
@@ -8,7 +8,13 @@ public readonly partial struct MongoId : IEquatable<MongoId>
 
     public MongoId(string id)
     {
-        if (string.IsNullOrWhiteSpace(id) || id.Length != 24)
+        // This is temporary, otherwise item buying is broken as when LINQ searches for string id's it's possible null is passed
+        if (id == null)
+        {
+            id = string.Empty;
+        }
+
+        if (id.Length != 24)
         {
             // TODO: Items.json root item has an empty parentId property
             Console.WriteLine($"Critical MongoId error: Incorrect length. id: {id}");
