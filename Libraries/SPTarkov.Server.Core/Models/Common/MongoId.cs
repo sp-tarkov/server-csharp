23c16cb433b4879c7e35442c0b9466062b870aea
Chomp <dev@dev.sp-tarkov.com>
2025-07-03 16:35:22 +0100

    Remove debug

--- Libraries/SPTarkov.Server.Core/Models/Common/MongoId.cs
+++ Libraries/SPTarkov.Server.Core/Models/Common/MongoId.cs
@@ -23,11 +23,6 @@ public readonly struct MongoId : IEquatable<MongoId>
             return;
         }
 
-        if (id == "hideout")
-        {
-            throw new Exception("wtf");
-        }
-
         if (id.Length != 24)
         {
             // TODO: Items.json root item has an empty parentId property
