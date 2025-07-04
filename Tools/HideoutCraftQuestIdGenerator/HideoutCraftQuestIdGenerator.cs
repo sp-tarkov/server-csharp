5c568967136b1aae9dea000517e07c0d92812d9f
Archangel <jesse@archangel.wtf>
2025-07-04 16:45:25 +0200

    Set template to mongo

--- Tools/HideoutCraftQuestIdGenerator/HideoutCraftQuestIdGenerator.cs
+++ Tools/HideoutCraftQuestIdGenerator/HideoutCraftQuestIdGenerator.cs
@@ -241,7 +241,7 @@ public class QuestProductionOutput
 {
     public MongoId QuestId { get; set; }
 
-    public string ItemTemplate { get; set; }
+    public MongoId ItemTemplate { get; set; }
 
     public double Quantity { get; set; }
 }
