f07bbb7bddb4b55078a0b9c51fe3492797732373
Alex <clodanSPT@hotmail.com>
2025-07-19 17:21:06 +0100

    Remove some extra traces of the old PreSptLoader

--- Libraries/SPTarkov.Server.Core/DI/OnLoadOrder.cs
+++ Libraries/SPTarkov.Server.Core/DI/OnLoadOrder.cs
@@ -3,16 +3,14 @@ namespace SPTarkov.Server.Core.DI;
 public static class OnLoadOrder
 {
     public const int Watermark = 0;
-    // public const int PreSptModLoader = 1000;
-    public const int Database = 2000;
-    public const int GameCallbacks = 3000;
-    public const int PostDBModLoader = 4000;
-    public const int TraderRegistration = 5000;
-    public const int HandbookCallbacks = 6000;
-    public const int HttpCallbacks = 7000;
-    public const int SaveCallbacks = 8000;
-    public const int TraderCallbacks = 9000;
-    public const int PresetCallbacks = 10000;
-    public const int RagfairCallbacks = 11000;
-    public const int PostSptModLoader = 12000;
+    public const int Database = 1000;
+    public const int GameCallbacks = 2000;
+    public const int PostDBModLoader = 3000;
+    public const int TraderRegistration = 4000;
+    public const int HandbookCallbacks = 5000;
+    public const int SaveCallbacks = 6000;
+    public const int TraderCallbacks = 7000;
+    public const int PresetCallbacks = 8000;
+    public const int RagfairCallbacks = 9000;
+    public const int PostSptModLoader = 10000;
 }
