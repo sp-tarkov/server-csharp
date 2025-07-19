6ef76fd08037eb43bc51870f4d419d3d96f71baa
Alex <clodanSPT@hotmail.com>
2025-07-19 17:48:48 +0100

    Re-introduced PreSptModLoad for now

--- Libraries/SPTarkov.Server.Core/DI/OnLoadOrder.cs
+++ Libraries/SPTarkov.Server.Core/DI/OnLoadOrder.cs
@@ -3,14 +3,15 @@ namespace SPTarkov.Server.Core.DI;
 public static class OnLoadOrder
 {
     public const int Watermark = 0;
-    public const int Database = 1000;
-    public const int GameCallbacks = 2000;
-    public const int PostDBModLoader = 3000;
-    public const int TraderRegistration = 4000;
-    public const int HandbookCallbacks = 5000;
-    public const int SaveCallbacks = 6000;
-    public const int TraderCallbacks = 7000;
-    public const int PresetCallbacks = 8000;
-    public const int RagfairCallbacks = 9000;
-    public const int PostSptModLoader = 10000;
+    public const int PreSptModLoader = 1000;
+    public const int Database = 2000;
+    public const int GameCallbacks = 3000;
+    public const int PostDBModLoader = 4000;
+    public const int TraderRegistration = 5000;
+    public const int HandbookCallbacks = 6000;
+    public const int SaveCallbacks = 7000;
+    public const int TraderCallbacks = 8000;
+    public const int PresetCallbacks = 9000;
+    public const int RagfairCallbacks = 10000;
+    public const int PostSptModLoader = 11000;
 }
