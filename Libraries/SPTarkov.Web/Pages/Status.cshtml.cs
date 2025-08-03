3209ddd8eabb058f8887b90b8a62ef9b4581909d
Archangel <jesse@archangel.wtf>
2025-08-03 19:57:48 +0200

    Add initial code for Razor pages support

---
+++ Libraries/SPTarkov.Web/Pages/Status.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SPTarkov.Web.Pages
+{
+    public class StatusModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
