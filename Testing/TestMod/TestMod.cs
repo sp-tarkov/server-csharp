5d2c423932fc7c95571c6b20e7a33dc0efc2a750
Cj <161484149+CJ-SPT@users.noreply.github.com>
2025-08-09 15:26:06 -0400

    Merge pull request #539 from CJ-SPT/test-mod
    Intoduce test mod and restructure test suites into their own solution folder

---
+++ Testing/TestMod/TestMod.cs
@@ -0,0 +1,32 @@
+﻿using SPTarkov.DI.Annotations;
+using SPTarkov.Server.Core.DI;
+using SPTarkov.Server.Core.Models.Spt.Mod;
+using SPTarkov.Server.Core.Models.Utils;
+
+namespace TestMod;
+
+public record TestModMetadata : AbstractModMetadata
+{
+    public override string ModGuid { get; init; } = "com.sp-tarkov.test-mod";
+    public override string Name { get; init; } = "test-mod";
+    public override string Author { get; init; } = "SPTarkov";
+    public override List<string>? Contributors { get; set; }
+    public override string Version { get; init; } = "1.0.0";
+    public override string SptVersion { get; init; } = "4.0.0";
+    public override List<string>? LoadBefore { get; set; }
+    public override List<string>? LoadAfter { get; set; }
+    public override List<string>? Incompatibilities { get; set; }
+    public override Dictionary<string, string>? ModDependencies { get; set; }
+    public override string? Url { get; set; }
+    public override bool? IsBundleMod { get; set; }
+    public override string? License { get; init; } = "MIT";
+}
+
+[Injectable(TypePriority = OnLoadOrder.PostDBModLoader + 1)]
+public class TestMod(ISptLogger<TestMod> logger) : IOnLoad
+{
+    public Task OnLoad()
+    {
+        return Task.CompletedTask;
+    }
+}
