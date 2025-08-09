849da303557e0c2fca21840ba88a72b4462dfb01
Cj <161484149+CJ-SPT@users.noreply.github.com>
2025-08-09 14:46:46 -0400

    add injectable to mod

--- Testing/TestMod/TestMod.cs
+++ Testing/TestMod/TestMod.cs
@@ -1,5 +1,7 @@
-﻿using SPTarkov.Server.Core.DI;
+﻿using SPTarkov.DI.Annotations;
+using SPTarkov.Server.Core.DI;
 using SPTarkov.Server.Core.Models.Spt.Mod;
+using SPTarkov.Server.Core.Models.Utils;
 
 namespace TestMod;
 
@@ -20,7 +22,8 @@ public record TestModMetadata : AbstractModMetadata
     public override string? License { get; init; } = "MIT";
 }
 
-public class TestMod : IOnLoad
+[Injectable(TypePriority = OnLoadOrder.PostDBModLoader + 1)]
+public class TestMod(ISptLogger<TestMod> logger) : IOnLoad
 {
     public Task OnLoad()
     {
