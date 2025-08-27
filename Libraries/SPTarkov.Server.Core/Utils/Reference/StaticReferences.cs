62b5fcef148336407b43e29f16f1115a8a612476
Alex <clodanSPT@hotmail.com>
2025-08-27 21:18:18 +0100

    Fixed patch for extension data

---
+++ Libraries/SPTarkov.Server.Core/Utils/Reference/StaticReferences.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SPTarkov.Server.Core.Utils.Reference;
+
+// This class is used by the JsonExtensionDataPatch, dont remove!
+public class StaticReferences
+{
+    [JsonIgnore]
+    private Dictionary<string, object> _reference = new();
+    [JsonExtensionData]
+    public Dictionary<string, object> Reference
+    {
+        get { return _reference; }
+    }
+}
