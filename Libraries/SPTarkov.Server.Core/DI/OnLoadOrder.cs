dca8d942157f5c419f50abe670850b57ca2f1bab
Chomp <dev@dev.sp-tarkov.com>
2025-09-04 20:04:31 +0100

    Expanded `OnLoadOrder` values

--- Libraries/SPTarkov.Server.Core/DI/OnLoadOrder.cs
+++ Libraries/SPTarkov.Server.Core/DI/OnLoadOrder.cs
@@ -3,15 +3,15 @@ namespace SPTarkov.Server.Core.DI;
 public static class OnLoadOrder
 {
     public const int Watermark = 0;
-    public const int PreSptModLoader = 1000;
-    public const int Database = 2000;
-    public const int GameCallbacks = 3000;
-    public const int PostDBModLoader = 4000;
-    public const int TraderRegistration = 5000;
-    public const int HandbookCallbacks = 6000;
-    public const int SaveCallbacks = 7000;
-    public const int TraderCallbacks = 8000;
-    public const int PresetCallbacks = 9000;
-    public const int RagfairCallbacks = 10000;
-    public const int PostSptModLoader = 11000;
+    public const int PreSptModLoader = 10000;
+    public const int Database = 20000;
+    public const int GameCallbacks = 30000;
+    public const int PostDBModLoader = 40000;
+    public const int TraderRegistration = 50000;
+    public const int HandbookCallbacks = 60000;
+    public const int SaveCallbacks = 70000;
+    public const int TraderCallbacks = 80000;
+    public const int PresetCallbacks = 90000;
+    public const int RagfairCallbacks = 100000;
+    public const int PostSptModLoader = 110000;
 }
