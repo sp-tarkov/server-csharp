96bc5ea55f91bead521b85b9af5e453850114e98
Chomp <dev@dev.sp-tarkov.com>
2025-09-04 20:07:30 +0100

    Made numbers larger so anime trader music mods can all fit in

--- Libraries/SPTarkov.Server.Core/DI/OnLoadOrder.cs
+++ Libraries/SPTarkov.Server.Core/DI/OnLoadOrder.cs
@@ -3,15 +3,15 @@ namespace SPTarkov.Server.Core.DI;
 public static class OnLoadOrder
 {
     public const int Watermark = 0;
-    public const int PreSptModLoader = 10000;
-    public const int Database = 20000;
-    public const int GameCallbacks = 30000;
-    public const int PostDBModLoader = 40000;
-    public const int TraderRegistration = 50000;
-    public const int HandbookCallbacks = 60000;
-    public const int SaveCallbacks = 70000;
-    public const int TraderCallbacks = 80000;
-    public const int PresetCallbacks = 90000;
-    public const int RagfairCallbacks = 100000;
-    public const int PostSptModLoader = 110000;
+    public const int PreSptModLoader = 100000;
+    public const int Database = 200000;
+    public const int GameCallbacks = 300000;
+    public const int PostDBModLoader = 400000;
+    public const int TraderRegistration = 500000;
+    public const int HandbookCallbacks = 600000;
+    public const int SaveCallbacks = 700000;
+    public const int TraderCallbacks = 800000;
+    public const int PresetCallbacks = 900000;
+    public const int RagfairCallbacks = 1000000;
+    public const int PostSptModLoader = 1100000;
 }
