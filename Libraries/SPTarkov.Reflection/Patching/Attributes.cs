1542bfb8d9c21d7055def267f307a564ba70c5ce
Cj <161484149+CJ-SPT@users.noreply.github.com>
2025-09-05 05:08:33 -0400

    Implement PatchManager (#585)
    * First pass at Fika's PatchManager implementation
    
    * add comments
    
    ---------
    
    Co-authored-by: Chomp <27521899+chompDev@users.noreply.github.com>

--- Libraries/SPTarkov.Reflection/Patching/Attributes.cs
+++ Libraries/SPTarkov.Reflection/Patching/Attributes.cs
@@ -14,3 +14,15 @@ public class PatchFinalizerAttribute : Attribute { }
 
 [AttributeUsage(AttributeTargets.Method)]
 public class PatchIlManipulatorAttribute : Attribute { }
+
+/// <summary>
+///     If added to a patch, it will not be used during auto patching
+/// </summary>
+[AttributeUsage(AttributeTargets.Class)]
+public class IgnoreAutoPatchAttribute : Attribute;
+
+/// <summary>
+///     If added to a patch, it will only be enabled during debug builds
+/// </summary>
+[AttributeUsage(AttributeTargets.Class)]
+public class DebugPatchAttribute : Attribute;
