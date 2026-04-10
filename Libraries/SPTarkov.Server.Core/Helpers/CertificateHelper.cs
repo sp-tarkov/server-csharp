ed7e6662e1e17b9cb9c72cae36e95df5ca0b0d22
Archangel <jesse@archangel.wtf>
2026-04-10 21:34:16 +0200

    Improve cert signing
    Loads certs as .crt now like it was in TS, doesn't load user's machine name as a subject name anymore

--- Libraries/SPTarkov.Server.Core/Helpers/CertificateHelper.cs
+++ Libraries/SPTarkov.Server.Core/Helpers/CertificateHelper.cs
@@ -12,94 +12,55 @@ public class CertificateHelper(ISptLogger<CertificateHelper> logger, FileUtil fi
 {
     private const string CertificatePath = "./user/certs/server.crt";
     private const string CertificateKeyPath = "./user/certs/server.key";
-    private const string CertificatePfxPath = "./user/certs/certificate.pfx";
 
-    //Todo: Finish off to match TS server
-    /// <summary>
-    ///     Not currently in use
-    /// </summary>
-    /// <returns></returns>
-    /// <exception cref="Exception"></exception>
-    public X509Certificate2 LoadOrGenerateCertificate()
+    public X509Certificate2? LoadOrGenerateCertificate()
     {
         if (!Directory.Exists("./user/certs"))
         {
             Directory.CreateDirectory("./user/certs");
         }
 
-        var certificate = LoadCertificate();
-
-        if (certificate == null)
-        {
-            // Generate self-signed certificate
-            certificate = GenerateSelfSignedCertificate("localhost");
-            SaveCertificate(certificate); // Save cert and new key
-            certificate = LoadCertificate();
-            if (certificate == null)
-            {
-                // if we are still null here there is a serious problem creating cert
-                throw new Exception("Certificate could not be loaded for the second time.");
-            }
-
-            logger.Success($"Generated and stored self-signed certificate ({CertificatePath})");
-        }
-
-        return certificate;
-    }
-
-    //Todo: When the above is finished off, remove any method with Pfx in the name
-    public X509Certificate2? LoadOrGenerateCertificatePfx()
-    {
-        if (!Directory.Exists("./user/certs"))
-        {
-            Directory.CreateDirectory("./user/certs");
-        }
-
-        if (TryLoadCertificatePfx(out var cert))
+        if (TryLoadCertificate(out var cert))
         {
             logger.Success($"Loaded self-signed certificate ({CertificatePath})");
             return cert;
         }
 
         // shit went wrong, throw a wobbly and close app
-        logger.Critical("Certificate pfx could not be loaded. Stopping server...");
+        logger.Critical("Certificate could not be loaded. Stopping server...");
         Environment.Exit(1);
         return null;
-    }
-
-    private X509Certificate2? LoadCertificate()
-    {
-        try
-        {
-            return X509Certificate2.CreateFromPemFile(CertificatePath, CertificateKeyPath);
-        }
-        catch (Exception)
-        {
-            return null;
-        }
     }
 
     /// <summary>
     ///     if the cert exist, try load it, else create one and try load again
     /// </summary>
     /// <returns></returns>
-    private bool TryLoadCertificatePfx(out X509Certificate2? certificate)
+    private bool TryLoadCertificate(out X509Certificate2 certificate)
     {
-        if (!File.Exists(CertificatePfxPath))
+        if (!File.Exists(CertificatePath) || !File.Exists(CertificateKeyPath))
         {
             // file doesn't exist so create straight away
-            var cert = GenerateSelfSignedCertificate("localhost");
-            SaveCertificatePfx(cert);
+            var cert = GenerateSelfSignedCertificate();
+            var privateKey = cert.GetRSAPrivateKey();
+
+            if (privateKey is null)
+            {
+                throw new InvalidOperationException("Private key is null");
+            }
+
+            SaveCertificate(cert);
+            SavePrivateKey(privateKey);
+
             logger.Success($"Generated and stored self-signed certificate ({CertificatePath})");
         }
 
         try
         {
-            // TODO:
-            //Archangel: For some reason despite this being deprecated this is the only way to load a certificate file
-            //No idea why, I want to eventually switch over to the other format so it lines up with the TS server
-            //But for now this works fine
-            certificate = new X509Certificate2(CertificatePfxPath);
+            var pem = X509Certificate2.CreateFromPemFile(CertificatePath, CertificateKeyPath);
+
+            // This badness has to exist so that .Net can actually use the cert for HTTPS
+            certificate = X509CertificateLoader.LoadPkcs12(pem.Export(X509ContentType.Pfx), null);
         }
         catch (Exception e)
         {
@@ -112,46 +73,22 @@ public class CertificateHelper(ISptLogger<CertificateHelper> logger, FileUtil fi
     }
 
     /// <summary>
-    ///     Get a certificate from provided path and return
-    /// </summary>
-    /// <returns>X509Certificate2</returns>
-    private X509Certificate2? LoadCertificatePfx()
-    {
-        try
-        {
-            //Archangel: For some reason despite this being deprecated this is the only way to load a certificate file
-            //No idea why, I want to eventually switch over to the other format so it lines up with the TS server
-            //But for now this works fine
-            return new X509Certificate2(CertificatePfxPath);
-        }
-        catch (Exception)
-        {
-            return null;
-        }
-    }
-
-    /// <summary>
     ///     Generate and return a self-signed certificate
     /// </summary>
-    /// <param name="subjectName">e.g. localhost</param>
     /// <returns>X509Certificate2</returns>
-    private X509Certificate2 GenerateSelfSignedCertificate(string subjectName)
+    private X509Certificate2 GenerateSelfSignedCertificate()
     {
         var sanBuilder = new SubjectAlternativeNameBuilder();
         sanBuilder.AddIpAddress(IPAddress.Loopback);
         sanBuilder.AddDnsName("localhost");
-        sanBuilder.AddDnsName(Environment.MachineName);
 
-        var distinguishedName = new X500DistinguishedName($"CN={subjectName}");
+        var distinguishedName = new X500DistinguishedName("CN=localhost");
 
         using var rsa = RSA.Create(2048);
         var request = new CertificateRequest(distinguishedName, rsa, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
         request.CertificateExtensions.Add(sanBuilder.Build());
 
-        //Todo: Enable when Pfx methods can be removed
-        //SavePrivateKey(rsa);
-
-        return request.CreateSelfSigned(new DateTimeOffset(DateTime.UtcNow.AddDays(-1)), new DateTimeOffset(DateTime.UtcNow.AddDays(3650)));
+        return request.CreateSelfSigned(new DateTimeOffset(DateTime.UtcNow.AddDays(-1)), new DateTimeOffset(DateTime.UtcNow.AddYears(99)));
     }
 
     /// <summary>
@@ -168,22 +105,6 @@ public class CertificateHelper(ISptLogger<CertificateHelper> logger, FileUtil fi
                 + Convert.ToBase64String(certificate.Export(X509ContentType.Cert), Base64FormattingOptions.InsertLineBreaks)
                 + "\n-----END CERTIFICATE-----";
             fileUtil.WriteFile(CertificatePath, certPem);
-        }
-        catch (Exception ex)
-        {
-            logger.Error($"Error saving certificate: {ex.Message}");
-        }
-    }
-
-    /// <summary>
-    ///     Save a certificate as a file to disk
-    /// </summary>
-    /// <param name="certificate">Certificate to save</param>
-    private void SaveCertificatePfx(X509Certificate2 certificate)
-    {
-        try
-        {
-            fileUtil.WriteFile(CertificatePfxPath, certificate.Export(X509ContentType.Pfx));
         }
         catch (Exception ex)
         {
--- SPTarkov.Server/Program.cs
+++ SPTarkov.Server/Program.cs
@@ -223,7 +223,7 @@ public static class Program
                         listenOptions.UseHttps(opts =>
                         {
                             opts.SslProtocols = SslProtocols.Tls12 | SslProtocols.Tls13;
-                            opts.ServerCertificate = certHelper.LoadOrGenerateCertificatePfx();
+                            opts.ServerCertificate = certHelper.LoadOrGenerateCertificate();
                             opts.ClientCertificateMode = ClientCertificateMode.NoCertificate;
                         });
                     }
