86204f0a29186f9b99509e267d520934b34605f4
Archangel <jesse@archangel.wtf>
2026-04-13 08:09:02 +0200

    Update comments

--- Libraries/SPTarkov.Server.Core/Models/Eft/Common/Vector2.cs
+++ Libraries/SPTarkov.Server.Core/Models/Eft/Common/Vector2.cs
@@ -2,6 +2,10 @@ using System.Text.Json.Serialization;
 
 namespace SPTarkov.Server.Core.Models.Eft.Common;
 
+/// <summary>
+/// This struct is used instead of <see cref="System.Numerics.Vector2"/>
+/// so the JSON representation can use lowercase member names ("x", "y").
+/// </summary>
 public readonly struct Vector2
 {
     [JsonPropertyName("x")]
--- Libraries/SPTarkov.Server.Core/Models/Eft/Common/Vector3.cs
+++ Libraries/SPTarkov.Server.Core/Models/Eft/Common/Vector3.cs
@@ -2,6 +2,10 @@ using System.Text.Json.Serialization;
 
 namespace SPTarkov.Server.Core.Models.Eft.Common;
 
+/// <summary>
+/// This struct is used instead of <see cref="System.Numerics.Vector3"/>
+/// so the JSON representation can use lowercase member names ("x", "y", "z").
+/// </summary>
 public readonly struct Vector3
 {
     [JsonPropertyName("x")]
