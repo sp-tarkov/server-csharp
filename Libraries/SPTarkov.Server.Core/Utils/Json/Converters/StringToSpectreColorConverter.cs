a0563b17f43814b8757b959296196f698c4edd30
cwx <cwxdev@outlook.com>
2026-07-15 12:32:27 +0100

    remove if block, for default

--- Libraries/SPTarkov.Server.Core/Utils/Json/Converters/StringToSpectreColorConverter.cs
+++ Libraries/SPTarkov.Server.Core/Utils/Json/Converters/StringToSpectreColorConverter.cs
@@ -6,43 +6,35 @@ namespace SPTarkov.Server.Core.Utils.Json.Converters;
 
 public class StringToSpectreColorConverter : JsonConverter<Color>
 {
-    // Colours the client has as an enum
-    private static readonly string[] _acceptableColors = ["Black", "Red", "Green", "Yellow", "Blue", "Magenta", "Cyan", "White", "Default", "Gray"];
-
     public override Color Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
         if (reader.TokenType == JsonTokenType.String)
         {
             var value = reader.GetString();
 
-            if (_acceptableColors.Contains(value))
+            switch (value)
             {
-                switch (value)
-                {
-                    case "Black":
-                        return Color.Black;
-                    case "Red":
-                        return Color.Red;
-                    case "Green":
-                        return Color.Green;
-                    case "Yellow":
-                        return Color.Yellow;
-                    case "Blue":
-                        return Color.Blue;
-                    case "Magenta":
-                        return Color.Magenta;
-                    case "Cyan":
-                        return Color.Cyan;
-                    case "White":
-                        return Color.White;
-                    case "Gray":
-                        return Color.Gray;
-                    default:
-                        return Color.Default;
-                }
+                case "Black":
+                    return Color.Black;
+                case "Red":
+                    return Color.Red;
+                case "Green":
+                    return Color.Green;
+                case "Yellow":
+                    return Color.Yellow;
+                case "Blue":
+                    return Color.Blue;
+                case "Magenta":
+                    return Color.Magenta;
+                case "Cyan":
+                    return Color.Cyan;
+                case "White":
+                    return Color.White;
+                case "Gray":
+                    return Color.Gray;
+                default:
+                    return Color.Default;
             }
-
-            throw new JsonException($"The Color given was not converted: {value}");
         }
 
         throw new JsonException($"The JsonTokenType was not of type string, it was: {reader.TokenType}");
